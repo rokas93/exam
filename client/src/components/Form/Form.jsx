567cba5ef5b7f2c598483480971e936450e8c079
Rokas <rokasgiedraitis@gmail.com>
2022-09-06 19:40:25 +0300

    Added formik logic to the form validation

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,83 +1,84 @@
 import { useState } from 'react';
 import { FormStyled } from './Form.styled';
 import { useDispatch } from 'react-redux';
+import { useFormik } from 'formik';
+import userValidation from '../../shared/userValidation';
 import { createUser } from '../../features/userSlice';
 import Button from '../Button';
 
 const Form = () => {
-  const [user, setUser] = useState({
-    name: '',
-    email: '',
-    date: '',
-    time: '',
-  });
   const [status, setStatus] = useState('idle');
   const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (status === 'idle') {
-      try {
-        setStatus('pending');
-
-        dispatch(createUser(user));
-
-        setStatus('idle');
-      } catch (error) {
-        console.log(error);
+  const { values, errors, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      date: '',
+      time: '',
+    },
+    validationSchema: userValidation,
+    onSubmit: (values) => {
+      if (status === 'idle') {
+        try {
+          setStatus('pending');
+          dispatch(createUser(values));
+          setStatus('idle');
+        } catch (error) {
+          console.log(error);
+        }
       }
-    }
-  };
+    },
+  });
 
   return (
     <FormStyled onSubmit={handleSubmit}>
       <label htmlFor='name'>
         Client name:
         <input
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev, name: e.target.value }))
-          }
           placeholder={'Name'}
           type='text'
           id='name'
+          onChange={handleChange}
+          value={values.name}
         />
+        {errors.name && <span>{errors.name}</span>}
       </label>
 
       <label htmlFor='email'>
         Client email:
         <input
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev, email: e.target.value }))
-          }
           placeholder={'email@email.com'}
           type='text'
           id='email'
+          onChange={handleChange}
+          value={values.email}
         />
+        {errors.email && <span>{errors.email}</span>}
       </label>
 
       <label htmlFor='date'>
         Reservation date:
         <input
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev, date: e.target.value }))
-          }
           type='date'
           id='date'
+          onChange={handleChange}
+          value={values.date}
         />
+        {errors.date && <span>{errors.date}</span>}
       </label>
 
       <label htmlFor='time'>
         Reservation time:
         <input
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev, time: e.target.value }))
-          }
           type='time'
           id='time'
           min={'08:00'}
           max={'17:00'}
+          onChange={handleChange}
+          value={values.time}
         />
+        {errors.time && <span>{errors.time}</span>}
       </label>
 
       <Button text={'Submit'} bg={'success'} />
