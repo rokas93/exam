15fc5426bd364a8dd34a0828a335bc9979802a25
Rokas <rokasgiedraitis@gmail.com>
2022-09-06 20:11:38 +0300

    Created Input styled component for ListItem

--- client/src/components/ListItem/ListItem.styled.jsx
+++ client/src/components/ListItem/ListItem.styled.jsx
@@ -27,6 +27,25 @@ export const ListItemStyled = styled.form`
   }
 `;
 
+export const InputStyled = styled.input`
+  outline: none;
+  border: none;
+  background-color: #fff;
+
+  border-radius: 0.375em;
+  color: #363636;
+  padding: 5px 10px;
+
+  justify-self: start;
+
+  ${({ isEdit }) =>
+    isEdit &&
+    css`
+      outline: 1px solid #485fc7;
+      box-shadow: 0 0 0 0.2em rgb(72 95 199 / 25%);
+    `}
+`;
+
 export const ButtonWrapperStyled = styled.div`
   display: flex;
   gap: 10px;
