57493308230ac96016939423d320c29755b7a5df
Rokas <rokasgiedraitis@gmail.com>
2022-09-06 17:42:55 +0300

    Added thunks logic for create, update and delete users

--- client/src/features/userSlice.js
+++ client/src/features/userSlice.js
@@ -1,15 +1,106 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+} from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = {
-  loading: false,
+const usersAdapter = createEntityAdapter({
+  selectId: (user) => user._id,
+});
+
+const initialState = usersAdapter.getInitialState({
   users: [],
-  error: '',
-};
+  status: 'idle',
+  error: null,
+});
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  try {
+    const { data } = await axios.get('http://localhost:5000/api/users');
+
+    return data;
+  } catch (error) {
+    return error.message;
+  }
+});
+
+export const createUser = createAsyncThunk(
+  'users/createUser',
+  async (newUser) => {
+    try {
+      const { data } = await axios.post(
+        'http://localhost:5000/api/users',
+        newUser
+      );
+
+      return data;
+    } catch (error) {
+      return error.message;
+    }
+  }
+);
+
+export const updateUser = createAsyncThunk(
+  'users/updateUser',
+  async (userData) => {
+    try {
+      const { data } = await axios.put(
+        `http://localhost:5000/api/users/${userData.id}`,
+        userData
+      );
+
+      return data;
+    } catch (error) {
+      return error.message;
+    }
+  }
+);
+
+export const deleteUser = createAsyncThunk(
+  'users/deleteUser',
+  async (userId) => {
+    try {
+      const { data } = await axios.delete(
+        `http://localhost:5000/api/users/${userId}`
+      );
+
+      return data;
+    } catch (error) {
+      return error.message;
+    }
+  }
+);
 
 const userSlice = createSlice({
-  name: 'user',
+  name: 'users',
   initialState,
+  reducers: {},
   extraReducers: (builder) => {
-    builder.addCase();
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.users.push(action.payload);
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        usersAdapter.upsertOne(state, action.payload);
+      })
+      .addCase(deleteUser.fulfilled, (state, action) => {
+        const id = action.payload._id;
+        usersAdapter.removeOne(state, id);
+        state.users = state.users.filter((user) => user._id !== id);
+      });
   },
 });
+
+export default userSlice.reducer;
