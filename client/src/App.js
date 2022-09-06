4a52320cdcdc906996d3064e4cf0ab144db8986d
Rokas <rokasgiedraitis@gmail.com>
2022-09-06 17:40:21 +0300

    Added Table component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Form from './components/Form';
 import List from './components/List';
+import Table from './components/Table';
 
 const App = () => {
   return (
-    <>
+    <Table>
       <Form />
       <List />
-    </>
+    </Table>
   );
 };
 
