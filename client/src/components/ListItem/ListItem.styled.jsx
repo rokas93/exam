74109e4dc957593c23aaef7fe9a0fcdc7f3200b6
Rokas <rokasgiedraitis@gmail.com>
2022-09-06 20:58:59 +0300

    Added styles for errors handling

--- client/src/components/ListItem/ListItem.styled.jsx
+++ client/src/components/ListItem/ListItem.styled.jsx
@@ -6,25 +6,6 @@ export const ListItemStyled = styled.form`
   gap: 10px;
 
   list-style: none;
-
-  & input {
-    outline: none;
-    border: none;
-    background-color: #fff;
-
-    border-radius: 0.375em;
-    color: #363636;
-    padding: 5px 10px;
-
-    justify-self: start;
-
-    ${({ isEdit }) =>
-      isEdit &&
-      css`
-        outline: 1px solid #485fc7;
-        box-shadow: 0 0 0 0.2em rgb(72 95 199 / 25%);
-      `}
-  }
 `;
 
 export const InputStyled = styled.input`
@@ -41,7 +22,7 @@ export const InputStyled = styled.input`
   ${({ isEdit }) =>
     isEdit &&
     css`
-      outline: 1px solid #485fc7;
+      outline: 1px solid ${({ isError }) => (isError ? '#f14668' : '#485fc7')};
       box-shadow: 0 0 0 0.2em rgb(72 95 199 / 25%);
     `}
 `;
