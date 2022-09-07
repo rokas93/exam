375cdb9cf49d7dad267776a8e583f633ed494572
Rokas <rokasgiedraitis@gmail.com>
2022-09-07 22:54:44 +0300

    Updated List component

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,12 +1,24 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchUsers } from '../../features/userSlice';
 import ListItem from '../ListItem';
-import { ListStyled } from './List.styled';
+import { ButtonStyled, ButtonsWrapperStyled, ListStyled } from './List.styled';
+import usePagination from '../../hooks/usePagination';
+import {
+  selectAllUsers,
+  getUsersStatus,
+  getUsersError,
+} from '../../features/userSlice';
 
 const List = () => {
   const dispatch = useDispatch();
-  const { users, status, error } = useSelector((state) => state.users);
+  const [page, setPage] = useState(0);
+
+  const users = useSelector(selectAllUsers);
+  const status = useSelector(getUsersStatus);
+  const error = useSelector(getUsersError);
+
+  const paginatedUsers = usePagination(users, 10);
 
   useEffect(() => {
     if (status === 'idle') {
@@ -19,12 +31,29 @@ const List = () => {
   if (status === 'loading') {
     content = <p>Loading...</p>;
   } else if (status === 'succeeded') {
-    content = users.map((user) => <ListItem key={user._id} user={user} />);
+    content = paginatedUsers[page].map((user) => (
+      <ListItem key={user._id} user={user} setPage={setPage} />
+    ));
   } else if (status === 'failed') {
     content = <p>{error}</p>;
   }
 
-  return <ListStyled>{content}</ListStyled>;
+  return (
+    <ListStyled>
+      {content}
+      <ButtonsWrapperStyled>
+        {paginatedUsers.map((_, index) => (
+          <ButtonStyled
+            key={index}
+            onClick={() => setPage(index)}
+            isActive={page === index}
+          >
+            {index + 1}
+          </ButtonStyled>
+        ))}
+      </ButtonsWrapperStyled>
+    </ListStyled>
+  );
 };
 
 export default List;
