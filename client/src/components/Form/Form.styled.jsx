cdd049a47d2dea94e0d1d20e9bb15e0bca18cc7c
Rokas <rokasgiedraitis@gmail.com>
2022-09-07 17:59:29 +0300

    Created Select styled component

--- client/src/components/Form/Form.styled.jsx
+++ client/src/components/Form/Form.styled.jsx
@@ -31,7 +31,36 @@ export const InputStyled = styled.input`
   padding: 5px 10px;
   margin-top: 10px;
 
+  box-shadow: ${({ isError }) =>
+    isError
+      ? '0 0 0 0.125em rgb(241 70 104 / 25%)'
+      : 'inset 0 0.0625em 0.125em rgb(10 10 10 / 5%)'};
+  background-color: #fff;
+
+  border: 1px solid ${({ isError }) => (isError ? '#f14668' : '#dbdbdb')};
+
+  border-radius: 0.375em;
+  color: #363636;
+
+  outline: none;
+
+  &:focus {
+    box-shadow: 0 0 0 0.125em rgb(72 95 199 / 25%);
+    border-color: #485fc7;
+  }
+`;
+
+export const SelectStyled = styled.select`
+  display: block;
+  padding: 5px 10px;
+  margin-top: 10px;
+
   box-shadow: inset 0 0.0625em 0.125em rgb(10 10 10 / 5%);
+
+  box-shadow: ${({ isError }) =>
+    isError
+      ? '0 0 0 0.125em rgb(241 70 104 / 25%)'
+      : 'inset 0 0.0625em 0.125em rgb(10 10 10 / 5%)'};
   background-color: #fff;
 
   border: 1px solid ${({ isError }) => (isError ? '#f14668' : '#dbdbdb')};
