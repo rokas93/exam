b7e4a94b141835745ca47d48da465f444d0cb200
Rokas <rokasgiedraitis@gmail.com>
2022-09-07 18:00:10 +0300

    Created Select styled component

--- client/src/components/ListItem/ListItem.styled.jsx
+++ client/src/components/ListItem/ListItem.styled.jsx
@@ -23,7 +23,32 @@ export const InputStyled = styled.input`
     isEdit &&
     css`
       outline: 1px solid ${({ isError }) => (isError ? '#f14668' : '#485fc7')};
-      box-shadow: 0 0 0 0.2em rgb(72 95 199 / 25%);
+      box-shadow: ${({ isError }) =>
+        isError
+          ? '0 0 0 0.2em rgb(241 70 104 / 25%)'
+          : '0 0 0 0.2em rgb(72 95 199 / 25%)'};
+    `}
+`;
+
+export const SelectStyled = styled.select`
+  outline: none;
+  border: none;
+  background-color: #fff;
+
+  border-radius: 0.375em;
+  color: #363636;
+  padding: 5px 10px;
+
+  justify-self: start;
+
+  ${({ isEdit }) =>
+    isEdit &&
+    css`
+      outline: 1px solid ${({ isError }) => (isError ? '#f14668' : '#485fc7')};
+      box-shadow: ${({ isError }) =>
+        isError
+          ? '0 0 0 0.2em rgb(241 70 104 / 25%)'
+          : '0 0 0 0.2em rgb(72 95 199 / 25%)'};
     `}
 `;
 
