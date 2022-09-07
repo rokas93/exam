588b33b6782f71e090eddfc32a03772f09575cc4
Rokas <rokasgiedraitis@gmail.com>
2022-09-07 22:55:00 +0300

    Updated List component styles

--- client/src/components/List/List.styled.jsx
+++ client/src/components/List/List.styled.jsx
@@ -1,8 +1,35 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const ListStyled = styled.div`
   display: flex;
   flex-direction: column;
 
   gap: 10px;
+`;
+
+export const ButtonStyled = styled.button`
+  border: 1px solid #dbdbdb;
+  border-radius: 0.375em;
+  color: #363636;
+
+  padding: 0.4em 0.8em;
+
+  background-color: #fff;
+
+  cursor: pointer;
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      background-color: #485fc7;
+      border-color: #485fc7;
+      color: #fff;
+    `}
+`;
+
+export const ButtonsWrapperStyled = styled.div`
+  display: flex;
+  gap: 10px;
+
+  align-self: center;
 `;
