33079edd50ba2c66cf3a69d8dd488db20bcf2890
Rokas <rokasgiedraitis@gmail.com>
2022-09-08 15:47:25 +0300

    Updated ListItem component

--- client/src/components/ListItem/ListItem.jsx
+++ client/src/components/ListItem/ListItem.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import {
   ButtonWrapperStyled,
   InputStyled,
+  LabelStyled,
   ListItemStyled,
   SelectStyled,
 } from './ListItem.styled';
@@ -13,48 +14,51 @@ import userValidation from '../../shared/userValidation';
 import RESERVATION_TIMES from '../../shared/constants/reservationTimes';
 import dateToSeconds from '../../shared/helpers/dateToSeconds';
 import { selectAllUsers } from '../../features/userSlice';
+import {
+  ClockIcon,
+  DateIcon,
+  EmailIcon,
+  UserIcon,
+  EditIcon,
+  CheckIcon,
+  XMarkIcon,
+} from '../../assets/svgs';
 
-const ListItem = ({ user, setPage }) => {
-  const [isEdit, setIsEdit] = useState(false);
+const ListItem = ({ user, setPage, setEditId, isEdit }) => {
+  const { _id, name, email, date, time } = user;
+
+  // -- States
   const [status, setStatus] = useState('idle');
   const [aviableTimes, setAviableTimes] = useState(null);
 
+  // Ref
+  const nameInput = useRef(null);
+  const dateInput = useRef(user.date);
+
+  // -- Redux
   const users = useSelector(selectAllUsers);
   const dispatch = useDispatch();
 
-  const { values, errors, handleChange, handleSubmit } = useFormik({
-    initialValues: {
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      date: user.date,
-      time: user.time,
-    },
-    validationSchema: userValidation,
-    onSubmit: (values) => {
-      setIsEdit(false);
-      setStatus('pending');
-      dispatch(updateUser(values));
-      setStatus('idle');
-    },
-  });
+  // -- Form validation
+  const { values, errors, handleChange, handleSubmit, setFieldValue } =
+    useFormik({
+      initialValues: {
+        _id,
+        name,
+        email,
+        date,
+        time,
+      },
+      validationSchema: userValidation,
+      onSubmit: (values) => {
+        setEditId(null);
+        setStatus('pending');
+        dispatch(updateUser(values));
+        setStatus('idle');
+      },
+    });
 
-  const handleDateChange = (currentDate, users) => {
-    if (currentDate) {
-      const results = RESERVATION_TIMES.filter((time) =>
-        users.every(
-          (user) =>
-            dateToSeconds(currentDate, time) !==
-            dateToSeconds(user.date, user.time)
-        )
-      );
-
-      setAviableTimes(results);
-    }
-
-    return;
-  };
-
+  // -- Handlers
   const handleDelete = (id) => {
     if (status === 'idle') {
       try {
@@ -70,77 +74,116 @@ const ListItem = ({ user, setPage }) => {
   };
 
   const handleEdit = () => {
-    setIsEdit(true);
-    values.time = '';
+    const results = RESERVATION_TIMES.filter((time) =>
+      users.every(
+        (user) =>
+          dateToSeconds(dateInput.current.value, time) !==
+          dateToSeconds(user.date, user.time)
+      )
+    );
+
+    setAviableTimes(results);
+    setEditId(user._id);
   };
 
+  const handleDateChange = () => {
+    if (values.date) {
+      setFieldValue('date', dateInput.current.value);
+      handleEdit();
+      setFieldValue('time', '');
+    }
+
+    return;
+  };
+
+  // Side effects
   useEffect(() => {
-    handleDateChange(values.date, users);
-  }, [values.date, users]);
+    if (isEdit) {
+      nameInput.current.focus();
+    }
+  }, [isEdit]);
 
   return (
     <ListItemStyled onSubmit={handleSubmit}>
-      <InputStyled
-        type='text'
-        id={'name'}
-        disabled={!isEdit}
-        isEdit={isEdit}
-        onChange={handleChange}
-        value={values.name}
-        isError={errors.name}
-      />
+      <LabelStyled htmlFor='name' isEdit={isEdit} isError={errors.name}>
+        <UserIcon width={'10px'} height={'10px'} />
+        <InputStyled
+          type='text'
+          id={'name'}
+          disabled={!isEdit}
+          onChange={handleChange}
+          value={values.name}
+          isError={errors.name}
+          ref={nameInput}
+        />
+      </LabelStyled>
 
-      <InputStyled
-        type='text'
-        id={'email'}
-        disabled={!isEdit}
-        isEdit={isEdit}
-        onChange={handleChange}
-        value={values.email}
-        isError={errors.email}
-      />
-      <InputStyled
-        type='date'
-        id={'date'}
-        disabled={!isEdit}
-        isEdit={isEdit}
-        onChange={handleChange}
-        value={values.date}
-        isError={errors.date}
-      />
+      <LabelStyled htmlFor='email' isEdit={isEdit} isError={errors.email}>
+        <EmailIcon width={'10px'} height={'10px'} />
+        <InputStyled
+          type='text'
+          id={'email'}
+          disabled={!isEdit}
+          onChange={handleChange}
+          value={values.email}
+          isError={errors.email}
+        />
+      </LabelStyled>
 
-      <SelectStyled
-        type='time'
-        id={'time'}
-        disabled={!isEdit}
-        isEdit={isEdit}
-        onChange={handleChange}
-        value={values.time}
-        isError={errors.time}
-      >
-        <option value={isEdit ? '' : values.time}>
-          {isEdit ? 'Choose time' : values.time}
-        </option>
+      <LabelStyled htmlFor='date' isEdit={isEdit} isError={errors.date}>
+        <DateIcon width={'10px'} height={'10px'} />
+        <InputStyled
+          type='date'
+          id={'date'}
+          disabled={!isEdit}
+          onChange={handleDateChange}
+          value={values.date}
+          isError={errors.date}
+          ref={dateInput}
+        />
+      </LabelStyled>
 
-        {aviableTimes?.map((time, index) => (
-          <option key={index} value={time}>
-            {time}
-          </option>
-        ))}
-      </SelectStyled>
+      <LabelStyled htmlFor='time' isEdit={isEdit} isError={errors.time}>
+        <ClockIcon width={'10px'} height={'10px'} />
+        <SelectStyled
+          type='time'
+          id={'time'}
+          disabled={!isEdit}
+          onChange={handleChange}
+          value={values.time}
+          isError={errors.time}
+        >
+          {values.time && <option value={values.time}>{values.time}</option>}
+          {!values.time && <option value={''}>{'Choose time'}</option>}
 
-      <ButtonWrapperStyled>
+          {aviableTimes?.map((time, index) => (
+            <option key={index} value={time}>
+              {time}
+            </option>
+          ))}
+        </SelectStyled>
+      </LabelStyled>
+
+      <ButtonWrapperStyled isEdit={isEdit}>
         <Button
           type={'button'}
-          text={!isEdit ? 'Edit' : 'Submit'}
+          text={
+            !isEdit ? (
+              <EditIcon height={'10px'} width={'10px'} />
+            ) : (
+              <CheckIcon height={'10px'} width={'10px'} />
+            )
+          }
           bg={'info'}
           action={!isEdit ? handleEdit : handleSubmit}
+          isRadio
         />
         <Button
           type={'button'}
-          text={'Delete'}
+          text={<XMarkIcon height={'10px'} width={'10px'} />}
           bg={'danger'}
-          action={() => handleDelete(user._id)}
+          action={() => handleDelete(_id)}
+          isRadio
         />
       </ButtonWrapperStyled>
     </ListItemStyled>
