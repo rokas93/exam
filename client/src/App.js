c035d199cd34a1f3208633a4466643b4e234a2f6
Rokas <rokasgiedraitis@gmail.com>
2022-09-08 15:45:07 +0300

    Added Footer and Header components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import Form from './components/Form';
+import React, { useState } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header/Header';
 import List from './components/List';
+import Modal from './components/Modal';
 import Table from './components/Table';
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Functions
+  const onOpen = () => setIsOpen(true);
+  const onClose = () => setIsOpen(false);
+
   return (
     <Table>
-      <Form />
+      <Header onOpen={onOpen} />
+
       <List />
+      {isOpen && <Modal onClose={onClose} />}
+
+      <Footer />
     </Table>
   );
 };
